dffc3fa001ed3c26bffe7b9ad2f4b815a95de0e3
trimiiz <168911053+trimiiz@users.noreply.github.com>
2026-01-31 03:27:00 +0100

    Delete pp3 wip directory
    new and improved incoming!

--- pp3 wip/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer class="main-footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> TechStore. All rights reserved.</p>
-        </div>
-    </footer>
-    <script src="assets/js/main.js"></script>
-</body>
-</html>
